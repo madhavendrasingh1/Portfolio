89dece2d3f1833485444fd6f7849bc3f77ee1e8a
Madhavendra Singh <126257657+madhavendrasingh1@users.noreply.github.com>
2025-03-07 11:07:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import About from './components/about';
+import About from './components/About';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
